225d33e725890cee082311b9d2e93165c9c4c589
Harriesh Gomahan <harrieshgomahangomahan@gmail.com>
2019-02-07 09:48:38 +0530

    Final

--- HTML5/LiveNotes.js
+++ HTML5/LiveNotes.js
@@ -7,7 +7,7 @@ function clickFunc(buttonName){
     var textAreaNote = textAreaObj.value;
 
     if(buttonName =='save'){
-        var fileName = prompt("Please enter name of the notes:");
+        var fileName = prompt("Please enter name of the notes to be saved:");
         window.localStorage.setItem(fileName,textAreaNote);
         var listTemp = document.createElement('li');
         listTemp.appendChild(document.createTextNode(fileName));
@@ -18,9 +18,14 @@ function clickFunc(buttonName){
     }else if(buttonName == 'new'){
 
 
+        textAreaObj.value="";
+
     }else if(buttonName == 'load'){
 
-
+        var fileName = prompt("Please enter name of the notes to be loaded:");
+        var noteContent = window.localStorage.getItem(fileName);
+        textAreaObj.value=noteContent;
+        alert("The notes has been loaded")
     }
 
 }
